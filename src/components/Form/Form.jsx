a2067eaacb0c386a5352fe1bccfee7682d7bdace
Michael Chukwuka Meninwa <chukwuka4christ@yahoo.com>
2026-01-04 22:19:10 -0500

    update form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -24,7 +24,11 @@ const Form = () => {
             
             <form onSubmit={handleSubmit} className="container">
                 <h1>Registration Form</h1>
-                {message && <h2>{message}</h2>}
+                {message && (
+                <h2 style={{ color: message === "All fields are required" ? "red" : "green" }}>
+                {message}
+                </h2>
+                )}
                 <label>Name: <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Enter your name"/></label>
                 <label>Email:<input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter your email"/></label>
                 <label>Password:<input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter your password"/></label>
